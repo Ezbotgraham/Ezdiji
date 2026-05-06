9a0c81f67de72c56bd16b3ac0710ef99deac2e9a
Ezbotgraham <grahamsmith363@gmail.com>
2026-05-06 07:54:08 +0300

    Improve backend bootstrap security

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from './app.module'
+import { corsConfig } from './config/cors.config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.enableCors()
+  app.enableCors(corsConfig)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  )
 
   await app.listen(3001)
 
