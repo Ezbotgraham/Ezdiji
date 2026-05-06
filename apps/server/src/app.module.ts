a45af4103878c48df257e3e4fd496086086ee416
Ezbotgraham <grahamsmith363@gmail.com>
2026-05-06 07:53:09 +0300

    Register voice gateway

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { Gateway } from './gateway/gateway'
 import { AuthModule } from './auth/auth.module'
 import { RoomsGateway } from './rooms/rooms.gateway'
+import { VoiceGateway } from './voice/voice.gateway'
 
 @Module({
   imports: [AuthModule],
-  providers: [Gateway, RoomsGateway],
+  providers: [Gateway, RoomsGateway, VoiceGateway],
 })
 export class AppModule {}
