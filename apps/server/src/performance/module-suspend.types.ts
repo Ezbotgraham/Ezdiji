02e0e03360c2fb991e76d79eda022f9a723ad97d
Ezbotgraham <grahamsmith363@gmail.com>
2026-05-06 08:34:45 +0300

    Add inactive module suspension system

---
+++ apps/server/src/performance/module-suspend.types.ts
@@ -0,0 +1,7 @@
+export interface ModuleSuspendState {
+  voiceSuspended: boolean
+  videoSuspended: boolean
+  streamSuspended: boolean
+  overlaySuspended: boolean
+  analyticsSuspended: boolean
+}
