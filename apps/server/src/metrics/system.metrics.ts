64808917979de2b99129cd838dc538be53fc21ce
Ezbotgraham <grahamsmith363@gmail.com>
2026-05-06 08:11:32 +0300

    Add system metrics model

---
+++ apps/server/src/metrics/system.metrics.ts
@@ -0,0 +1,9 @@
+export interface SystemMetrics {
+  cpuUsage: number
+  memoryUsage: number
+  websocketConnections: number
+  activeVoiceUsers: number
+  activeStreams: number
+  uploadBandwidth: number
+  downloadBandwidth: number
+}
