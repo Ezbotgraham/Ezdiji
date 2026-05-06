dac7b0d3d0e2bea105d53438bd5dc70bb08c9308
Ezbotgraham <grahamsmith363@gmail.com>
2026-05-06 07:46:04 +0300

    Add auth service

---
+++ apps/server/src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import * as bcrypt from 'bcrypt'
+
+export class AuthService {
+  async hashPassword(password: string) {
+    return bcrypt.hash(password, 12)
+  }
+
+  async verifyPassword(password: string, hash: string) {
+    return bcrypt.compare(password, hash)
+  }
+}
