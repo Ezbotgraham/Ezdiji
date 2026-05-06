753a929b3bf0e6878b499ec6e79343451f3455ce
Ezbotgraham <grahamsmith363@gmail.com>
2026-05-06 07:58:11 +0300

    Add admin permission system

---
+++ apps/server/src/admin/admin.permissions.ts
@@ -0,0 +1,12 @@
+export const ADMIN_PERMISSIONS = {
+  USER_BAN: true,
+  USER_MUTE: true,
+  USER_KICK: true,
+  ROOM_CREATE: true,
+  ROOM_DELETE: true,
+  ROOM_LOCK: true,
+  ROOM_UNLOCK: true,
+  EVENT_CREATE: true,
+  EVENT_DELETE: true,
+  SCREEN_FORCE_STOP: true,
+}
