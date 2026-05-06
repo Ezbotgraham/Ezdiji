fc090af4adb881867bde4ecbf6cea7a18cef40db
Ezbotgraham <grahamsmith363@gmail.com>
2026-05-06 07:52:31 +0300

    Register room gateway

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { Gateway } from './gateway/gateway'
 import { AuthModule } from './auth/auth.module'
+import { RoomsGateway } from './rooms/rooms.gateway'
 
 @Module({
   imports: [AuthModule],
-  providers: [Gateway],
+  providers: [Gateway, RoomsGateway],
 })
 export class AppModule {}
