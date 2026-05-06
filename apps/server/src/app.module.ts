4e7d935ca794fd263a42cda2cc869a159c92ddab
Ezbotgraham <grahamsmith363@gmail.com>
2026-05-06 07:54:49 +0300

    Register screen and remote gateways

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,9 +3,17 @@ import { Gateway } from './gateway/gateway'
 import { AuthModule } from './auth/auth.module'
 import { RoomsGateway } from './rooms/rooms.gateway'
 import { VoiceGateway } from './voice/voice.gateway'
+import { ScreenGateway } from './screen/screen.gateway'
+import { RemoteGateway } from './remote/remote.gateway'
 
 @Module({
   imports: [AuthModule],
-  providers: [Gateway, RoomsGateway, VoiceGateway],
+  providers: [
+    Gateway,
+    RoomsGateway,
+    VoiceGateway,
+    ScreenGateway,
+    RemoteGateway,
+  ],
 })
 export class AppModule {}
